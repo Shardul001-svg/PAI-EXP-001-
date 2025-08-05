63304e23a9a2b1ecb443ce7edd818741f465fae9
Shardul001-svg <sharduldeshpande700@gmail.com>
2025-08-05 18:29:57 +0530

    Update Hello.asm

--- Hello.asm
+++ Hello.asm
@@ -1,19 +1,18 @@
 section .data
-    msg db "Hello, World!", 0xA  ; The string + newline character
-    len equ $ - msg              ; Calculate length of string
+    msg db "Hello, World!", 0xA 
+    len equ $ - msg              
 
 section .text
     global _start
 
 _start:
-    ; sys_write (stdout, msg, len)
-    mov eax, 4          ; syscall number for sys_write
-    mov ebx, 1          ; file descriptor 1 = stdout
-    mov ecx, msg        ; pointer to message
-    mov edx, len        ; length of message
-    int 0x80            ; make syscall
+    mov eax, 4        
+    mov ebx, 1         
+    mov ecx, msg     
+    mov edx, len      
+    int 0x80           
 
-    ; sys_exit (0)
-    mov eax, 1          ; syscall number for sys_exit
-    xor ebx, ebx        ; exit code 0
-    int 0x80            ; make syscall
+
+    mov eax, 1         
+    xor ebx, ebx      
+    int 0x80           
